ded48eec37b1d865ca98887c99ea09c93c98955c
Kahnu Charan Swain <djkanha044@gmail.com>
2023-04-12 10:29:28 +0530

    Update Panel

--- authorize-net-for-contact-form-7.php
+++ authorize-net-for-contact-form-7.php
@@ -9,3 +9,38 @@
  * Author URI: https://github.com/kahnu044/
  * GitHub Plugin URI: https://github.com/kahnu044/authorize-net-for-contact-form-7
  */
+
+
+
+/**
+ * Adds custom panels to the Contact Form 7 form editor.
+ *
+ * @param array $panels An array of existing panels in the form editor.
+ * @return array An array of modified panels that includes the custom panels.
+ */
+
+add_filter('wpcf7_editor_panels', 'afcf7_register_panel');
+
+function afcf7_register_panel($panels)
+{
+
+    $panels['afcf7-settings-panel'] = array(
+        'title'    => __('Authorize.Net Settings', 'contact-form-7-authorize-net-addon'),
+        'callback' => 'afcf7_additional_settings',
+    );
+
+    return $panels;
+}
+
+
+/**
+ * Adding Authorize.Net fields in Authorize.Net Settings tab
+ *
+ * @param $cf7
+ */
+
+function afcf7_additional_settings($cf7)
+{
+
+
+}
