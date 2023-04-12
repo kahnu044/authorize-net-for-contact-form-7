a8b3268872323a2c5f62ae753b8d55e033408255
Kahnu Charan Swain <djkanha044@gmail.com>
2023-04-12 09:44:26 +0530

    Plugin Details Updated

---
+++ authorize-net-for-contact-form-7.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Authorize.net For Contact Form 7
+ * Plugin URI: https://github.com/kahnu044/authorize-net-for-contact-form-7
+ * Description: Handles Payments For Contact Form 7 Using Authorize.net
+ * Version: 1.0.0
+ * Author: Kahnu044
+ * Author URI: https://github.com/kahnu044/
+ * GitHub Plugin URI: https://github.com/kahnu044/authorize-net-for-contact-form-7
+ */
