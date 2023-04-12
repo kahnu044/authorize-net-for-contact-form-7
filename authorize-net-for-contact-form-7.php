580de7eb73d726a63d58471cc716b268c8a53048
Kahnu Charan Swain <djkanha044@gmail.com>
2023-04-12 16:01:34 +0530

    Style and Js added

--- authorize-net-for-contact-form-7.php
+++ authorize-net-for-contact-form-7.php
@@ -11,13 +11,32 @@
  */
 
 
-
 define('AFCF7_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('AFCF7_TEMPLATE_PATH', plugin_dir_path(__FILE__) . 'template/');
 define('AFCF7_PLUGIN_URL', plugins_url('', __FILE__));
 define('AFCF7_ASSETS_URL',  plugins_url('assets/', __FILE__));
 define('AFCF7_PLUGIN_VERSION',  '1.0.0');
-define('AFCF7_META_PREFIX', 'afcf7_' );
+define('AFCF7_META_PREFIX', 'afcf7_');
+
+/**
+ * Enqueue the custom style and script to the plugin.
+ */
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('afcf7-styles', AFCF7_ASSETS_URL . 'css/style.css', array(), AFCF7_PLUGIN_VERSION, 'all');
+    wp_enqueue_script('afcf7-script', AFCF7_ASSETS_URL . 'js/script.js', array('jquery'), AFCF7_PLUGIN_VERSION, true);
+});
+
+
+add_action('admin_enqueue_scripts', function () {
+
+    // Only for contact form 7 edit pages
+    if (isset($_GET['page']) && $_GET['page'] === 'wpcf7') {
+        wp_enqueue_style('afcf7-admin-styles', AFCF7_ASSETS_URL . 'css/admin.style.css', array(), AFCF7_PLUGIN_VERSION, 'all');
+        wp_enqueue_script('afcf7-admin-script', AFCF7_ASSETS_URL . 'js/admin-script.js', array('jquery'), AFCF7_PLUGIN_VERSION, true);
+    }
+});
+
 
 /**
  * Adds custom panels to the Contact Form 7 form editor.
@@ -49,5 +68,5 @@ function afcf7_register_panel($panels)
 function afcf7_additional_settings($cf7)
 {
 
-    require_once(AFCF7_TEMPLATE_PATH .'afcf7-panel-settings.php');
+    require_once(AFCF7_TEMPLATE_PATH . 'afcf7-panel-settings.php');
 }
